b3e9b2250906691ad343fd663e65ca0c3ff7d3b4
The-Aldi-Tri <the.aldi.tri@gmail.com>
2023-11-07 11:12:17 +0700

    Configure Helmet Using the ‘parent’ helmet() Middleware

--- myApp.js
+++ myApp.js
@@ -5,21 +5,35 @@ const app = express();
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: "deny" }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
+
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({ action: "deny" }));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// app.use(
+//   helmet.hsts({ maxAge: 90 * 24 * 60 * 60 /*90dayInSeconds*/, force: true })
+// );
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       scriptSrc: ["'self'", "trusted-cdn.com"],
+//     },
+//   })
+// );
+
+//Helmet all above but in one app.use
 app.use(
-  helmet.hsts({ maxAge: 90 * 24 * 60 * 60 /*90dayInSeconds*/, force: true })
-);
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "trusted-cdn.com"],
+  helmet({
+    noCache: true,
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "trusted-cdn.com"],
+      },
     },
   })
 );
