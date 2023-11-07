ecfc97544286796ef23bab96f5a176318ca5c19a
The-Aldi-Tri <the.aldi.tri@gmail.com>
2023-11-07 10:40:35 +0700

    Prevent IE from Opening Untrusted HTML with helmet.ieNoOpen()

--- myApp.js
+++ myApp.js
@@ -9,6 +9,7 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 app.disable("strict-transport-security");
 app.use("/_api", api);
 app.get("/", function (request, response) {
